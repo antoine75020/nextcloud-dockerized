379b5ce4a9c2c98a6c02143ce7d1084b20d5eff7
antoine75020 <54755498+antoine75020@users.noreply.github.com>
2021-07-05 19:30:37 +0200

    Add files via upload

---
+++ nextcloud/config.php
@@ -0,0 +1,20 @@
+<?php
+
+$CONFIG = array (
+  'trusted_domains' =>
+  array (
+    0 => 'nextcloud.your_domain.tld'
+  ),
+
+  'trusted_proxies' =>
+  array (
+    0 => '172.18.0.x'
+  ),
+
+  'default_language' => 'fr',
+  'default_locale' => 'fr_FR',
+  'default_phone_region' => 'FR',
+  'defaultapp' => 'files,dashboard',
+  'logtimezone' => 'Europe/Paris',
+
+);
